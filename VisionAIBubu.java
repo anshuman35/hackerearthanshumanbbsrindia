d838411f7eb4b438c4f896c0b47ae491cb988833
anshuman35 <49508586+anshuman35@users.noreply.github.com>
2023-01-11 17:20:51 +0530

    Create VisionAIBubu.java

---
+++ VisionAIBubu.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.math.*;
+import java.applet.*;
+public class Vision_AI extends Applet{
+
+public void draw(Graphics zf)
+{
+zf.drawString(“<c-wiz=zd> <button class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-dgl2Hf ksBjEc lKxP2d LQeN7 c6Oyec" jscontroller="soHxf" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:Jy”);
+}
+
+
+static public void main(String[] arg){
+ArrayList hju = new ArrayList();
+hju.add(“
+
+
+<c-wiz=zd> <button class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-dgl2Hf ksBjEc lKxP2d LQeN7 c6Oyec" jscontroller="soHxf" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue; touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc; touchcancel:JMtRjd; focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef;mlnRJb:fLiPzd" data-idom-class="ksBjEc lKxP2d LQeN7 c6Oyec" jsname="X6q4ye" aria-label="Categories">
+");
+        String ghr = "00b5fc2d3153637e65a35363295e14ba.jpg";
+        System.out.println(ghr+""+hju);
+        
+        StringTokenizer kdx = new StringTokenizer(ghr);
+        try
+        {
+          StringTokenizer hjl = new StringTokenizer(" 00b5bf7dce31145b95ef3adb89a99b03.jpg");
+          StringTokenizer hjl = new StringTokenizer("000b0fd767c5ab2656b7ed8d43674777.jpg");
+          int hnx = Integer.parseInt("1"+""+hjl);
+          System.out.println(hnx);
+        }
+        catch(Exception ch){
+          
+          System.out.println(ch);
+        }
+        
+        
+        }
+        }
